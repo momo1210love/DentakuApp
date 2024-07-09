494f414666055ef7fbfe45ae75727780b380c389
momo1210love <momo1210love@gmail.com>
2024-07-10 00:51:03 +0900

    colors変更

--- app/src/main/java/com/example/dentakuapp/MainActivity.java
+++ app/src/main/java/com/example/dentakuapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView inputText;
+    private AppCompatButton inputAppCompatButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
         inputText = findViewById(R.id.input);
+        inputText = findViewById(R.id.output);
+
+        inputAppCompatButton = findViewById(R.id.bottom_clear);
+        inputAppCompatButton = findViewById(R.id.bottom_bracket_left);
+        inputAppCompatButton = findViewById(R.id.bottom_bracket_right);
+        inputAppCompatButton = findViewById(R.id.bottom_division);
+        inputAppCompatButton = findViewById(R.id.bottom_7);
+        inputAppCompatButton = findViewById(R.id.bottom_8);
+        inputAppCompatButton = findViewById(R.id.bottom_9);
+        inputAppCompatButton = findViewById(R.id.bottom_multiply);
+        inputAppCompatButton = findViewById(R.id.bottom_4);
+        inputAppCompatButton = findViewById(R.id.bottom_5);
+        inputAppCompatButton = findViewById(R.id.bottom_6);
+        inputAppCompatButton = findViewById(R.id.bottom_subtraction);
+        inputAppCompatButton = findViewById(R.id.bottom_1);
+        inputAppCompatButton = findViewById(R.id.bottom_2);
+        inputAppCompatButton = findViewById(R.id.bottom_3);
+        inputAppCompatButton = findViewById(R.id.bottom_addition);
+        inputAppCompatButton = findViewById(R.id.bottom_0);
+        inputAppCompatButton = findViewById(R.id.bottom_dot);
+        inputAppCompatButton = findViewById(R.id.bottom_equals);
+
     }
 }
 
