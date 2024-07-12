897baacdabd8f3f271cc5740f404bcc509b241ee
momo1210love <momo1210love@gmail.com>
2024-07-13 01:41:05 +0900

    no message

--- app/src/main/java/com/example/dentakuapp/MainActivity.java
+++ app/src/main/java/com/example/dentakuapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.dentakuapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -10,10 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView inputText;
-    private AppCompatButton inputAppCompatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +30,45 @@ public class MainActivity extends AppCompatActivity {
         inputText = findViewById(R.id.input);
         inputText = findViewById(R.id.output);
 
-        inputAppCompatButton = findViewById(R.id.bottom_clear);
-        inputAppCompatButton = findViewById(R.id.bottom_bracket_left);
-        inputAppCompatButton = findViewById(R.id.bottom_bracket_right);
-        inputAppCompatButton = findViewById(R.id.bottom_division);
-        inputAppCompatButton = findViewById(R.id.bottom_7);
-        inputAppCompatButton = findViewById(R.id.bottom_8);
-        inputAppCompatButton = findViewById(R.id.bottom_9);
-        inputAppCompatButton = findViewById(R.id.bottom_multiply);
-        inputAppCompatButton = findViewById(R.id.bottom_4);
-        inputAppCompatButton = findViewById(R.id.bottom_5);
-        inputAppCompatButton = findViewById(R.id.bottom_6);
-        inputAppCompatButton = findViewById(R.id.bottom_subtraction);
-        inputAppCompatButton = findViewById(R.id.bottom_1);
-        inputAppCompatButton = findViewById(R.id.bottom_2);
-        inputAppCompatButton = findViewById(R.id.bottom_3);
-        inputAppCompatButton = findViewById(R.id.bottom_addition);
-        inputAppCompatButton = findViewById(R.id.bottom_0);
-        inputAppCompatButton = findViewById(R.id.bottom_dot);
-        inputAppCompatButton = findViewById(R.id.bottom_equals);
+        findViewById(R.id.bottom_clear).setOnClickListener(buttonListener);
+        findViewById(R.id.bottom_bracket_left).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_bracket_right).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_division).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_7).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_8).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_9).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_multiply).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_4).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_5).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_6).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_subtraction).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_1).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_2).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_3).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_addition).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_0).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.bottom_dot).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.bottom_equals).setOnClickListener(buttonOperatorListener);
 
     }
+    View.OnClickListener buttonNumberListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+    View.OnClickListener buttonOperatorListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
 
 //package com.example.dentakuapp;
